068d2eb77095ba454510e560c3b79bd81574cc1c
xiweizhao118 <xiweizhao118@outlook.com>
2024-05-15 20:46:26 +0300

    update

--- execute_action.py
+++ execute_action.py
@@ -1,5 +1,7 @@
 import subprocess
-
+import openai, json
+model_id = 'model-id'
+openai.api_key = 'open-api-key'
 
 def run_terminal_command(command):
     '''
@@ -15,15 +17,36 @@ def run_terminal_command(command):
     except Exception as e:
         print("An error occurred:", e)
 
+def user_language_demands():
+    user_input = input("How can I help you today? ")  # Get user input
+    # feed the user input to our fine-tuned model
+    test_messages = [
+        {"role": "system", "content": "You are a robot assistant that can extract keywords from user's demands and help us save the keywords and corresponding parameters into a dictionary format."},
+        {"role": "user", "content": user_input}
+    ]
+    completion = openai.ChatCompletion.create(
+    model=model_id,
+    messages=test_messages
+    )
+    gpt_reply = completion.choices[0].message
+    # get the reply (dictionary format) from the model
+    reply_dict = json.loads(gpt_reply['content'])
+
+    # print(reply_dict)
+    return user_input, reply_dict["what"]
 
 # Main function
 def main():
-    # Process the instructions
-    file_name = "cube"
-    # instruction = '''ros2 run ros2_execution ros2_execution.py --ros-args -p PROGRAM_FILENAME:="cube" -p ROBOT_MODEL:="irb120" -p EE_MODEL:="schunk"'''
-    instruction = f'''ros2 run ros2_execution ros2_execution.py --ros-args -p PROGRAM_FILENAME:="{file_name}" -p ROBOT_MODEL:="irb120" -p EE_MODEL:="schunk"'''
-    run_terminal_command(instruction)
-    print("Task completed successfully!")
+    while True:
+        user_input, item_name = user_language_demands()
+        if user_input == "exit":
+            break
+        command = f'''ros2 run ros2_execution ros2_execution.py --ros-args -p PROGRAM_FILENAME:="{item_name}" -p ROBOT_MODEL:="irb120" -p EE_MODEL:="schunk"'''
+        # run_terminal_command(command)
+        print(command)
+        print("Task completed successfully!")
+    
+    print("Goodbye!")
 
 
 if __name__ == "__main__":
